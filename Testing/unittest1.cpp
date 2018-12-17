b5e671f7a8dd4f8b42189da303b1ae0d6a49674e
MrLever <MrLeverx@gmail.com>
2018-12-17 01:43:32 -0500

    Set up unit test framework

---
+++ Testing/unittest1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testing{		
+
+	TEST_CLASS(UnitTest1){
+	public:
+		
+		TEST_METHOD(TestMethod1){
+			Assert::AreEqual(1, 1);
+		}
+
+	};
+}
