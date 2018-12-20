c096bbe121a178f021791bbab49d3e6d4873ed8c
MrLever <MrLeverx@gmail.com>
2018-12-20 16:49:02 -0500

    Fix unit testing Framework

---
+++ ProjectCoatiTestSuite/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Source/Game.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectCoatiTestSuite
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Game test("Test");
+			// TODO: Your test code here
+		}
+
+	};
+}
