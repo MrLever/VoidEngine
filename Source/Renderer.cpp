a00373ec039a95326d9665d5bea034b94d6dbd4e
MrLever <MrLeverx@gmail.com>
2018-12-22 16:01:24 -0500

    Hook renderer into window context

--- Source/Renderer.cpp
+++ Source/Renderer.cpp
@@ -1,11 +1,34 @@
+//STD Headers
+#include <memory>
+
+//Library Headers
+#include "glad/glad.h"
+
+//Coati Headers
 #include "Renderer.h"
+#include "WindowManager.h"
 
 
 
-Renderer::Renderer(){
+
+Renderer::Renderer(std::shared_ptr<WindowManager> Window) {
+	this->Window = Window;
+
 }
 
 
-Renderer::~Renderer()
-{
+Renderer::~Renderer() {
+}
+
+//Private Member Functions
+
+
+
+//Public Member Functions
+
+void Renderer::Render() {
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	Window->SwapBuffers();
 }
--- Source/Renderer.h
+++ Source/Renderer.h
@@ -1,7 +1,30 @@
 #pragma once
+//STD Headers
+#include <memory>
+
+//Library Headers
+
+
+//Forward Class declarations
+class WindowManager;
+
 class Renderer{
+private:
+	//Private class members
+	std::shared_ptr<WindowManager> Window;
+
 public:
-	Renderer();
+	//CTORS
+	Renderer(std::shared_ptr<WindowManager> Window);
 	~Renderer();
+
+private:
+	//Private member functions
+
+
+public:
+	//Public member functions
+	void Render();
+
 };
 
