31651b350bb13d02a79737703c8c01aa237f1519
DogtorPhD <lectrodic@gmail.com>
2018-12-23 22:59:38 -0500

    Created a Utilities file and added in a FNV-1a hash function

---
+++ Source/Utilities.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//STD Headers
+#include <string>
+
+
+//Library Headers
+
+
+
+static unsigned int FNV1aHash(std::string input) {
+
+	int FNV_prime = 1099511628211;
+
+	int hash = 14695981039346656037;
+
+	for (int i = 0; i < input.length(); i++) {
+		
+		hash = hash xor (int)input[i];
+		hash = hash * FNV_prime;
+	}
+
+	return hash;
+
+}
