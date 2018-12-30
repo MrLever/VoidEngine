561a63e26fb17676f3f18ef57615efc3c9e8aa5e
MrLever <nicolacognata@gmail.com>
2018-12-30 00:25:06 -0500

    Speculative fixes type issues in FNV1a hash function

--- Source/EngineUtilities.h
+++ Source/EngineUtilities.h
@@ -7,19 +7,19 @@
 //Library Headers
 
 namespace EngineUtilities {
-	static unsigned int FNV1aHash(std::string input) {
+	static unsigned FNV1aHash(std::string input) {
 
-		int FNV_prime = 1099511628211;
+		auto FNV_prime = 1099511628211;
 
-		int hash = 14695981039346656037;
+		auto hash = 14695981039346656037;
 
 		for (int i = 0; i < input.length(); i++) {
 
-			hash = hash xor (int)input[i];
+			hash = hash ^ static_cast<unsigned>(input[i]);
 			hash = hash * FNV_prime;
 		}
 
-		return hash;
+		return static_cast<unsigned>(hash);
 
 	}
 }
