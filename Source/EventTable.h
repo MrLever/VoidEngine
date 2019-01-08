83f56ffb7060d3cf2db2e6bd4bc57c8d9a6c2425
Nic <nicolacognata@gmail.com>
2019-01-07 21:25:56 -0500

    Add EventTable data structure to project

---
+++ Source/EventTable.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//STD Headers
+#include <unordered_map>
+#include <functional>
+
+//Library Headers
+
+//Coati Headers
+
+//Forward Class declarations
+
+
+class EventTable{
+private:
+	//Private Class Members
+	std::unordered_map<Message, std::function<void()>> Events;
+
+public:
+	//CTORS
+	EventTable();
+	~EventTable();
+
+	std::function<void()> operator[] (const Message& event) const;
+
+
+private:
+	//Private member functions
+
+public:
+	//Public member functions
+	void BindEvent(Message message, std::function<void()> event);
+	void UnbindEvent(Message message);
+};
+
