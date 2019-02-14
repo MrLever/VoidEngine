3915a29e6514c4b8e872eabb44adf128e08783ef
Nic <nicolacognata@gmail.com>
2019-02-14 15:46:45 -0500

    Add unit tests for event tables

--- ProjectCoatiTestSuite/EventTableTests.cpp
+++ ProjectCoatiTestSuite/EventTableTests.cpp
@@ -4,8 +4,8 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+
 #include "..\Source\EventTable.h"
-#include "..\Source\Message.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -14,22 +14,56 @@ namespace ProjectCoatiTestSuite {
 
 	TEST_CLASS(EventTableTests) {
 	public:
+
+
+
 		TEST_METHOD(BindEventTest) {
 			EventTable dummyTable;
 			Message dummyMessage("Test", MessageType::GenericEvent);
-			dummyTable.BindEvent()
+			
+			bool dummyFlag = false;
+			dummyTable.BindEvent(
+				dummyMessage, 
+				[&]() -> void {
+					dummyFlag = true;
+				}
+			);
 
-			Assert::IsTrue(false);
+			//Run the lambda expression definded above to flip the flag
+			dummyTable[dummyMessage]();
+
+			Assert::IsTrue(dummyFlag);
 		}
 
 		TEST_METHOD(UnbindEventTest) {
+			EventTable dummyTable;
+			Message dummyMessage("Test", MessageType::GenericEvent);
 
-			Assert::IsTrue(false);
+			
+			dummyTable.BindEvent(
+				dummyMessage,
+				[]() -> void {
+					std::cout << "Testing\n";
+				}
+			);
+
+			dummyTable.UnbindEvent(dummyMessage);
+
+			Assert::IsTrue(dummyTable[dummyMessage] == nullptr);
 		}
 
 		TEST_METHOD(IndexOperatorTest) {
+			EventTable dummyTable;
+			Message dummyMessage("Test", MessageType::GenericEvent);
 
-			Assert::IsTrue(false);
+			dummyTable.BindEvent(
+				dummyMessage,
+				[]() -> void {
+				
+				}
+			);
+
+			Assert::IsTrue(dummyTable[dummyMessage] != nullptr);
 		}
 
 	};
