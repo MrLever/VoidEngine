4f34aa6f30759d47103adba4ce975ba82444f8a4
Nic <nicolacognata@gmail.com>
2019-03-20 19:28:41 -0400

    Update test suite

--- ProjectCoatiTestSuite/InputManagerTests.cpp
+++ ProjectCoatiTestSuite/InputManagerTests.cpp
@@ -6,19 +6,25 @@
 
 #include "..\Source\MessageBus.h"
 #include "..\Source\InputManager.h"
+#include "TestingUtilities.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 
 namespace ProjectCoatiTestSuite {
-
 	TEST_CLASS(InputManagerTests) {
 	public:
 		TEST_METHOD(TriggerKeybindingTest) {
 			std::shared_ptr<MessageBus> dummyBus = std::make_shared<MessageBus>();
 			InputManager dummyManager(dummyBus);
+			DummyReceiver dummyReciever(dummyBus);
+			KeyboardInput dummyInput(KeyType::ESC, KeyState::Pressed);
+			
 
-			Assert::IsTrue(false);
+			dummyManager.HandleInput(dummyInput);
+			dummyBus->DispatchMessages();
+
+			Assert::IsTrue(dummyReciever.EventFired);
 		}
 
 	};
--- ProjectCoatiTestSuite/MessageBusTests.cpp
+++ ProjectCoatiTestSuite/MessageBusTests.cpp
@@ -7,55 +7,13 @@
 #include "..\Source\MessageBus.h"
 #include "..\Source\MessageBusNode.h"
 #include "..\Source\Message.h"
+#include "TestingUtilities.h"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 
 namespace ProjectCoatiTestSuite
 {
-	class DummyReceiver : MessageBusNode {
-	public:
-		bool MessageReceived;
-		bool EventFired;
-		std::shared_ptr<MessageBus> bus;
-		
-		DummyReceiver(std::shared_ptr<MessageBus> bus) : MessageBusNode(bus){
-			this->bus = bus;
-			MessageReceived = false;
-			EventFired = false;
-			this->RegisterReciever();
-			this->RegisterEvents();
-		}
-		~DummyReceiver() {
-
-		}
-
-		void RegisterEvents() override {
-			//Events.BindEvent(Message("FireTestEvent", MessageType::Input), &FireEvent);
-			auto eventLambda = [=]() -> void {
-				this->FireEvent();
-			};
-
-			Events.BindEvent(Message("FireTestEvent", MessageType::Input), eventLambda);
-		}
-
-		void RegisterReciever() override {
-			bus->AddReceiver(this, MessageType::Input);
-		}
-		
-		void ReceiveMessage(Message message) override {
-			MessageReceived = true;
-			if (Events[message]) {
-				Events[message]();
-			}
-
-		}
-
-		void FireEvent() {
-			std::cout << "Event Fired";
-			EventFired = true;
-		}
-	};
-
 	TEST_CLASS(MessageBusTests) {
 	public:
 
