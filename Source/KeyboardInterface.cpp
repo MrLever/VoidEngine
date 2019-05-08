5f6ed34ff002c0d0019ae062644f749fd4156089
Nic <nicolacognata@gmail.com>
2019-05-08 02:22:47 -0400

    Add keyboard interface files to project

---
+++ Source/KeyboardInterface.cpp
@@ -0,0 +1,19 @@
+//STD Headers
+
+//Library Headers
+
+//Coati Headers
+#include "KeyboardInterface.h"
+
+
+namespace EngineCore {
+
+	KeyboardInterface::KeyboardInterface() {
+
+	}
+
+	KeyboardInterface::~KeyboardInterface() {
+
+	}
+
+}
---
+++ Source/KeyboardInterface.h
@@ -0,0 +1,31 @@
+#pragma once
+//STD Headers
+#include <queue>
+
+//Library Headers
+
+//Coati Headers
+
+namespace EngineCore {
+
+	class KeyboardInterface {
+	private:
+		//Private Class Members
+		
+	public:
+		//tors
+		KeyboardInterface();
+		~KeyboardInterface();
+	
+	private:
+		//private member functions
+	
+	public:
+		//public member functions
+
+	};
+
+	
+	
+
+}
