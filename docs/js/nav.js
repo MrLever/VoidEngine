c60071c7d9a85c83b5e646e39ba07017f3199844
MrLever <nicolacognata@gmail.com>
2019-05-10 19:05:12 -0400

    Fill out documentation framework

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -1,5 +1,6 @@
 //Loads navigation menu into each page on wiki.
 //Allows for central management of navigation
+//This function does not work locally. Only on the server.
 function loadNav(){
     console.log("Loading navigation");
     fetch('js/nav.html').then(
@@ -16,6 +17,6 @@ function loadNav(){
         document.getElementById('NavDiv').innerHTML = html;
     })
     .catch(function(err){
-        console.log('Fetch error :\'( ' + err);
+        document.getElementById('NavDiv').innerHTML = 'oops, couldn\'t load navigation';
     });
 }
