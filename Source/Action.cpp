5ac360219b2ebc60d66aaede4073bc5b4a1030b8
Nic <nicolacognata@gmail.com>
2019-05-23 11:28:23 -0400

    Fill out Action class

--- Source/Action.cpp
+++ Source/Action.cpp
@@ -1,13 +1,58 @@
+//STD Headers
+
+//Library Headers
+
 //Coati headers
 #include "Action.h"
+#include "EngineUtilities.h"
 
 namespace EngineCore {
 
-	Action::Action() {
+	//tors
+	Action::Action(std::string name, ActionType type) : ActionName(name), Type(type) {
+		ActionID = EngineUtilities::FNV1aHash(name);
+		Event = nullptr;
+	}
+
+	Action::Action(std::string name, ActionType type, std::function<void()> event) : ActionName(name), Type(type), Event(event){
+		ActionID = EngineUtilities::FNV1aHash(name);
 	}
 
 
 	Action::~Action() {
+
 	}
+
+	bool Action::operator==(const Action& other) const {
+		return this->ActionID == other.ActionID;
+	}
+
+	//Public Member Functions
+	std::string Action::GetName() const {
+		return ActionName;
+	}
+
+	unsigned long long Action::GetID() const {
+		return ActionID;
+	}
+
+	ActionType Action::GetAction() {
+		return Type;
+	}
+
+	void Action::BindAction(std::function<void()> newAction) {
+		this->Event = newAction;
+	}
+
+	void Action::TriggerAction() {
+		if (Event == nullptr) {
+			std::cerr << "Error, no action bound";
+			return;
+		}
+
+		Event();
+	}
+
+
 
 }
--- Source/Action.h
+++ Source/Action.h
@@ -1,17 +1,46 @@
 #pragma once
 //STD Headers
+#include <string>
+#include <functional>
+#include <iostream>
 
 //Library Headers
 
 //Coati Headers
 
 namespace EngineCore {
+	enum class ActionType : unsigned {
+		Discrete = 1,
+		Continuous = 2,
+	};
 
 	class Action {
+	
+	private:
+		std::string ActionName;
+		unsigned long long ActionID;
+
+		ActionType Type;
+
+		std::function<void()> Event;
 
 	public:
-		Action();
+		Action(std::string name, ActionType type);
+		Action(std::string name, ActionType type, std::function<void()> event);
 		~Action();
+
+		bool operator==(const Action& other) const;
+
+	private:
+
+	public:
+		std::string GetName() const;
+		unsigned long long GetID() const;
+		ActionType GetAction();
+
+		void BindAction(std::function<void()> newAction);
+		void TriggerAction();
+		
 
 	};
 
