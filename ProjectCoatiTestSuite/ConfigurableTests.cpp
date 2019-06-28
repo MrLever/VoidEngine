d85d0490ac5d98743fa2c127e232426916425ccd
Nic <nicolacognata@gmail.com>
2019-06-27 20:43:02 -0400

    Add unit test suite for Configurable objects

---
+++ ProjectCoatiTestSuite/ConfigurableTests.cpp
@@ -0,0 +1,25 @@
+#include <memory>
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "TestingUtilities.h"
+#include "Configurable.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace EngineCore;
+
+namespace ProjectCoatiTestSuite {
+
+	class DummyConfigurable : Configurable {
+		DummyConfigurable() : Configurable("testConfig.lua") {
+		}
+	};
+
+	TEST_CLASS(ConsoleTests) {
+public:
+	TEST_METHOD(ToggleOutputTest) {
+		
+	}
+	};
+}
