2c6f07c8fccbefc2d7d629e78f67bce024a3da3f
MrLever <nicolacognata@gmail.com>
2019-07-06 15:20:30 -0400

    Add UUID test suite

---
+++ EngineTestSuite/UUIDTests.cpp
@@ -0,0 +1,22 @@
+//STD Headers
+
+//Library Headers
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+//SuperVoid Headers
+#include "UUID.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace EngineTestSuite {
+
+	TEST_CLASS(UUIDTests) {
+		public:
+
+		TEST_METHOD(UUIDConstructorTest) {
+
+		}
+	};
+};
