2a2de572982560e0c3e49749679c0dc969e401da
Nic <nicolacognata@gmail.com>
2019-07-06 21:25:46 -0400

    Add thread pool to engine core

---
+++ EngineCore/ThreadPool.h
@@ -0,0 +1,17 @@
+#pragma once
+//STD Headers
+
+//Library Headers
+
+//Void Engine Headers
+
+namespace EngineUtils {
+
+	/**
+	 * The threadpool is responsible for accepting and executing jobs asynchronously for engine systems.
+	 */
+	class ThreadPool {
+
+	};
+
+}
