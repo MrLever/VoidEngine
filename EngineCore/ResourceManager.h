34174ccd8a136f9c12ffc93e9bd570039ef1a269
Nic <nicolacognata@gmail.com>
2019-07-12 20:19:57 -0400

    Ponder the meaning of all this

--- EngineCore/ResourceManager.h
+++ EngineCore/ResourceManager.h
@@ -1,12 +1,15 @@
 #pragma once
 //STD Headers
+#include <filesystem>
 #include <memory>
-#include <unordered_set>
+#include <unordered_map>
 
 //Library Headers
 
 //Void Engine Headers
 #include "ThreadPool.h"
+#include "Resource.h"
+#include "UUID.h"
 
 namespace EngineUtils {
 
@@ -16,6 +19,9 @@ namespace EngineUtils {
 	 */
 	class ResourceManager {
 	public:
+		template <typename P>
+		using ResourceHandle = std::future<std::shared_ptr<P>>;
+		
 		///CTORS
 		/**
 		 * Constructor
@@ -29,10 +35,26 @@ namespace EngineUtils {
 		~ResourceManager();
 
 		///Public Member Functions
+		/**
+		 * Function to command the resource manager to load a resource.
+		 * If the resource has already been loaded, it will not be loaded again
+		 */
+		/*template <typename T> 
+		ResourceHandle<T> LoadResource(std::filesystem::path resourceLocation);*/
+
 	
 	private:
 		///Private Member Variables
+		/** The thread pool the Resource Manager depends on for async file IO */
 		std::shared_ptr<ThreadPool> GameThreadPool;
+
+		/** Registry of all loaded resources */
+		std::unordered_map<UUID, Resource> ResourceRegistry;
 	};
+
+	//template<typename T>
+	//inline ResourceManager::ResourceHandle<T> ResourceManager::LoadResource(std::filesystem::path resourceLocation) {
+	//	return ResourceHandle<T>();
+	//}
 }
 
