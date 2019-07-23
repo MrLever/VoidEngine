ac92d70749d0998f09c2169f13b35cb5c37976d1
Nic <nicolacognata@gmail.com>
2019-07-23 00:09:33 -0400

    Add LuaScript class to replace Configuration class and luabridge

---
+++ EngineCore/LuaScript.cpp
@@ -0,0 +1,5 @@
+#include "LuaScript.h"
+
+namespace EngineUtils {
+
+}
---
+++ EngineCore/LuaScript.h
@@ -0,0 +1,16 @@
+#pragma once
+#pragma once
+//STD Headers
+
+//Library Headers
+
+//Void Engine Headers
+
+
+namespace EngineUtils {
+	
+	class LuaScript {
+
+	};
+
+}
