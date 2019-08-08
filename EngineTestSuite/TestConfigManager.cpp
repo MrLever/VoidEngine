7d6ce697c42db384f3e98ccee22467e6c78e3cff
Nic <nicolacognata@gmail.com>
2019-08-07 23:27:20 -0400

    Add ConfigManager tests

---
+++ EngineTestSuite/TestConfigManager.cpp
@@ -0,0 +1,22 @@
+//STD Headers
+#include <memory>
+
+//Library Headers
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+//Void Engine Headers
+#include "ConfigManager.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EngineUtilitiesTests {
+	using namespace EngineUtils;
+
+	TEST_CLASS(ConfigManagerTests) {
+	public:
+		TEST_METHOD(ConfigManagerConstructionTest) {
+			Assert::Fail();
+		}
+	};
+}
