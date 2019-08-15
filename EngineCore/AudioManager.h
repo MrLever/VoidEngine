9a39707446b8f79bb7e960d4b700ec6abf19b80f
Nic <nicolacognata@gmail.com>
2019-08-14 21:59:09 -0400

    Update doxygen in AudioManager.h

--- EngineCore/AudioManager.h
+++ EngineCore/AudioManager.h
@@ -16,10 +16,21 @@ namespace EngineCore {
 	class AudioManager : public EngineUtils::Configurable {
 	public:
 		///CTORS
+		/**
+		 * Constructor
+		 * @param engineInterface Shared pointer to the engine utility interface this system will use
+		 * @param configFile The file location of the configuration file this system will use
+		 */
 		AudioManager(EngineInterfacePtr engineInterface, const std::string& configFile);
+		
+		/**
+		 * Destructor
+		 */
 		~AudioManager() = default;
 
-
+		/**
+		 * Configures the object using the settings in the config file
+		 */
 		void Configure() override;
 
 	private:
