0793e5e2fcaa82f71844340af55eaaf5cf118a67
MrLever <nicolacognata@gmail.com>
2019-09-02 00:13:27 -0400

    Add boilerplate for future Time Utilities

--- EngineCore/TimeUtils.cpp
+++ EngineCore/TimeUtils.cpp
@@ -12,8 +12,13 @@ namespace EngineUtils {
 	GameTime GetGameTime() {
 		auto timeSinceEpoch = std::chrono::duration_cast<std::chrono::milliseconds>(
 			std::chrono::system_clock::now().time_since_epoch()
-			);
+		);
 
 		return timeSinceEpoch.count();
+	}
+
+	std::string GetLocalTime(){
+		//TODO(MrLever): Use new c++20 Calendar stuff when it comes out
+		return "ERROR";
 	}
 }
