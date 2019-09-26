04ea1400a07aa55be0e3e6f43df4bd2e02eca25b
Nic <nicolacognata@gmail.com>
2019-09-25 22:02:18 -0400

    Add camera components to EngineCore

---
+++ EngineCore/CameraComponent.cpp
@@ -0,0 +1,51 @@
+//STD Headers
+
+//Library Headers
+
+//Void Engine Headers
+#include "CameraComponent.h"
+
+namespace EngineCore {
+
+	CameraComponent::CameraComponent(Entity* parent) : Component(parent), ProjectionMatrix(1), ViewMatrix(1), FOV(45.0) {
+		UpdateProjectionMatrix();
+	}
+
+	CameraComponent::~CameraComponent() {
+	}
+
+	void CameraComponent::Tick(float deltaTime) {
+		Position = Parent->GetPostion();
+
+		auto target = Position + LookDirection;
+		ViewMatrix = glm::lookAt(
+			glm::vec3(Position.X, Position.Y, Position.Z),
+			glm::vec3(target.X, target.Y, target.X),
+			glm::vec3(Up.X, Up.Y, Up.Z)
+	    );
+
+	}
+
+	glm::mat4 CameraComponent::GetViewMatrix() const {
+		return ViewMatrix;
+	}
+
+	void CameraComponent::SetFOV(float fov) {
+		FOV = fov;
+		
+		UpdateProjectionMatrix();
+	}
+
+	void CameraComponent::UpdateProjectionMatrix() {
+		auto windowWidth = WindowManager::GetActiveWindow()->GetWindowWidth();
+		auto windowHeight = WindowManager::GetActiveWindow()->GetWindowWidth();
+
+		ProjectionMatrix = glm::perspective<float>(
+			glm::radians(FOV),
+			(float)(windowWidth / windowHeight),
+			0.1f,
+			100.0f
+		);
+	}
+
+}
---
+++ EngineCore/CameraComponent.h
@@ -0,0 +1,66 @@
+#pragma once
+//STD Headers
+#include <vector>
+
+//Library Headers
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+//Void Engine Headers
+#include "Component.h"
+#include "Vector.h"
+#include "WindowManager.h"
+
+namespace EngineCore {
+	class CameraComponent : public Component {
+	public:
+		/**
+		 * Constructor
+		 * @param parent This component's parent entity
+		 */
+		CameraComponent(Entity* parent);
+
+		/**
+		 * Destructor
+		 */
+		~CameraComponent();
+
+		/**
+		 * Defines how a component processes updates
+		 * @param deltaSeconds the time elapsed since the previous tick
+		 */
+		virtual void Tick(float deltaTime);
+
+		/**
+		 * Allows caller to access this camera's view matrix
+		 * @return ViewMatrix
+		 */
+		glm::mat4 GetViewMatrix() const;
+
+		/**
+		 * Allows user to set this camera's FOV
+		 */
+		void SetFOV(float fov);
+
+	private:
+		/**
+		 * Helper function to update the camera's Projection Matrix
+		 */
+		void UpdateProjectionMatrix();
+
+		/** This camera's projection matrix */
+		glm::mat4 ProjectionMatrix;
+
+		/** This camera's view matrix */
+		glm::mat4 ViewMatrix;
+
+		float FOV;
+
+		EngineMath::Vector3 LookDirection;
+		
+		EngineMath::Vector3 Up;
+	};
+
+}
