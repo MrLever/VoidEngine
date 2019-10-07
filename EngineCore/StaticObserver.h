47cf1cc5279a600ee521bded706e829d3fd1f4b2
Nic <nicolacognata@gmail.com>
2019-10-07 00:19:59 -0400

    Add static observer

---
+++ EngineCore/StaticObserver.h
@@ -0,0 +1,39 @@
+#pragma once
+//STD Headers
+
+//Library Headers
+
+//Void Engine Headers
+#include "Entity.h"
+
+namespace core {
+
+	/**
+	 * @class StaticObserver
+	 * @brief A Static Observer is a camera weilding entity that stands absolutely still
+	 */
+	class StaticObserver : public Entity{
+	public:
+		/**
+		 * Constructor
+		 */
+		StaticObserver(const utils::Name& name);
+
+		/**
+		 * Constructor
+		 */
+		StaticObserver(const std::string& name);
+
+		/**
+         * Pure virtual function that defines what the entity does once a level begins.
+		 * @note This function largely acts like an initialization function
+		 */
+		void BeginPlay() override;
+
+		/**
+		 * Defines entity death behaviors
+		 */
+		virtual void Terminate();
+	};
+
+}
