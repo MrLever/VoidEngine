102874a7aad1297081700db70a3ac5dba4a213ee
Nic <nicolacognata@gmail.com>
2019-10-11 23:06:20 -0400

    Add missed file rename

---
+++ EngineCore/InputAxisReport.cpp
@@ -0,0 +1,15 @@
+//STD Headers
+
+//Library Headers
+
+//Void Engine Headers
+#include "InputAxisReport.h"
+
+namespace core {
+
+	InputAxisReport::InputAxisReport(const std::string& name, float value) 
+		: AxisName(name), Value(value) {
+
+	}
+
+}
